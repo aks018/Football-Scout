f8ca3399b2f6b2f67f5c73857ee48ff301f030f4
Avitansh <aks018@ucsd.edu>
2018-08-02 12:02:21 -0700

    Creating Android Tests using Espresso

--- app/src/androidTest/java/aviee/footballscout/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/aviee/footballscout/ExampleInstrumentedTest.java
@@ -2,11 +2,21 @@ package aviee.footballscout;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.Espresso;
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withContentDescription;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static org.junit.Assert.*;
 
 /**
@@ -16,11 +26,52 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<MainActivity>(MainActivity.class);
+
+
+    private MainActivity mainActivity = null;
+
+    @Before
+    public void setUp() throws Exception {
+        mainActivity = mActivityTestRule.getActivity();
+    }
+
     @Test
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
         assertEquals("aviee.footballscout", appContext.getPackageName());
+    }
+
+    @Test
+    public void testNewPlayerProfileImageButtonIsDisplayed() {
+
+        onView(withId(R.id.newPlayerProfileButton)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void testSavedPlayerProfileImageButtonIsDisplayed() {
+
+        onView(withId(R.id.savedPlayerProfilesButton)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void testSettingsButtonIsDisplayed() {
+
+        onView(withId(R.id.settingsButton)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void testSettingsButtonContentDescription() {
+        onView(withId(R.id.settingsButton)).check(matches(withContentDescription(R.string.settingsImageButtonDescription)));
+    }
+
+
+    @After
+    public void tearDown() throws Exception {
+        mainActivity = null;
     }
 }
